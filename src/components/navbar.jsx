327ef7613281b4ee3fa85c3cc3b7a37ee67d8f40
nicolasbauzaSenpai <nicolasbauza21@gmail.com>
2023-05-05 12:36:49 -0300

    changes

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -51,6 +51,7 @@ const Navbar = () => {
           isScrolled &&
           "linear-gradient(90deg, rgba(12,90,117,1) 0%, rgba(1,103,166,1) 100%)",
         background: !isScrolled && "rgb(12,90,117)",
+        justifyContent: "space-between",
       }}
     >
       <Container maxWidth="xl">
@@ -74,53 +75,6 @@ const Navbar = () => {
               textDecoration: "none",
             }}
           ></Typography>
-
-          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
-            <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
-              color="inherit"
-              sx={{
-                display: { xs: "flex", md: "none", justifyContent: "flex-end" },
-              }}
-            >
-              <MenuIcon />
-            </IconButton>
-
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: "bottom",
-                horizontal: "left",
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: "top",
-                horizontal: "left",
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-              sx={{
-                display: { xs: "block", md: "none" },
-              }}
-            >
-              {pages.map((page) => (
-                <MenuItem
-                  component="a"
-                  key={page}
-                  href={`#${page.toLowerCase()}`}
-                  onClick={handleCloseNavMenu}
-                >
-                  <Typography textAlign="center">{page}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
-
           <Box
             sx={{
               flexGrow: 1,
@@ -130,14 +84,13 @@ const Navbar = () => {
             {pages.map((page) => (
               <Button
                 key={page}
-                component="a"
                 href={`#${page.toLowerCase()}`}
                 onClick={handleCloseNavMenu}
                 sx={{
                   my: 2,
                   color: "white",
                   display: "block",
-                  justifyContent: "flex-end",
+                  justifyContent: "space-between",
                 }}
               >
                 {page}
